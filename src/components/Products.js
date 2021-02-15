4e50823670a3e62eea7bbe85f160a5ca400c8a6e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-02-15 19:59:25 +0100

    edit form

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Products = () => {
 
     return (
-        <h1>From Products</h1>
+        <Fragment>
+            <h2 className="text-center my-5">Products</h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                       <th scope="col">Name</th> 
+                       <th scope="col">Price</th> 
+                       <th scope="col">Options</th> 
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </Fragment>
     );
 }
 
